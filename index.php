22e029450a8ad138e68b74246ffe85b5a5d61f51
Grant Trudel <grant.trudel91@gmail.com>
2023-05-15 18:13:00 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,29 +1,32 @@
 <div id="myDIV" class="header">
   <h2>My To Do List</h2>
-  <form action="myform2.php" method="post">
+  <form action="controller.php" method="post">
 	  <input type="text" name="myInput" id="myInput" placeholder="Title...">
-	  <input type="submit" name="submit" value=Add />
+	  <input type="submit" name="submit" value="Add" />
+  </form>
+  <form action="controller.php" method="post">
+	  <input type="submit" name="submit" value="Reset" />
   </form>
 </div>
 <?php
 session_start();
-$todolist = ['run','read','write'];
-if (!empty($_SESSION['test2'])) {
-	foreach ($_SESSION['test2'] as $item) {
+if (empty($_SESSION['todolist'])) $_SESSION['todolist'] = ['run','read','write'];
+if (!empty($_SESSION['todolist'])) {
+	foreach ($_SESSION['todolist'] as $item) {
 		$todolist[] = $item;
 	}
 }
 ?>
-<form action="myform.php" method="post">
+<form action="controller.php" id="form1" method="post">
 <ul id="myUL">
 <?php foreach ($todolist as $key => $item) {
 	$checked = '';
-	if (!empty($_SESSION['test'][$item])) $checked = 'checked';
-	echo '<li><input name="'.$item.'" type="checkbox" id="'.$item.'" value="'.$item.'" '.$checked.'>'.$item.'</li>';
+	if (!empty($_SESSION['checkeditems'][$item])) $checked = 'checked';
+	echo '<li><input name="'.$item.'" type="checkbox" id="'.$item.'" value="'.$item.'" '.$checked.' onchange=document.getElementById(\'form1\').submit() >'.$item.'</li>';
 }
 ?>
 </ul>
-<input type="submit" name="submit" value="Submit" />
+<!-- input type="submit" name="submit" value="Submit" /-->
 </form>
 
 
